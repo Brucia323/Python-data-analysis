b0534989ca1489fc89f7e272a31742935f2ca1b6
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-02 17:51:57 +0800

    Update Untitled-1.py

--- Experiment-1/Untitled-1.py
+++ Experiment-1/Untitled-1.py
@@ -1,20 +1,24 @@
-#Febonacci sequence
+# Febonacci sequence
 '''
 斐波那契数列
 输入：项数n
 输出：前n项
 '''
 import os
+
+
 def fibo(n):
     numbers = [1, 1]
     for i in range(n - 2):
         numbers.append(numbers[i] + numbers[i + 1])
     return numbers
+
+
 answer = fibo(10)
 print(answer)
 if not os.path.exists('result'):
     os.mkdir('result')
-file=open('result/fibo.txt','w')
+file = open('result/fibo.txt', 'w')
 for num in answer:
     file.write(str(num)+' ')
 file.close()
