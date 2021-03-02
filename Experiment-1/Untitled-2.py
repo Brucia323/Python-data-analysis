3172cca1af5e33dc56b83b6190c1081610ed2e57
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-02 17:51:59 +0800

    Update Untitled-2.py

--- Experiment-1/Untitled-2.py
+++ Experiment-1/Untitled-2.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import random
-gradewords=('优秀','良好','中等','及格','不及格')
-testwords=[random.choice(gradewords) for i in range(1000)]
-result=dict()
+gradewords = ('优秀', '良好', '中等', '及格', '不及格')
+testwords = [random.choice(gradewords) for i in range(1000)]
+result = dict()
 for item in testwords:
     if item in testwords:
-        result[item]=result.get(item,0)+1
-for key,v in result.items():
-    print(key,v,sep='--->')
-plt.pie(result.values(),labels=result.keys(),autopct='%.2f%%')
+        result[item] = result.get(item, 0)+1
+for key, v in result.items():
+    print(key, v, sep='--->')
+plt.pie(result.values(), labels=result.keys(), autopct='%.2f%%')
 plt.show()
