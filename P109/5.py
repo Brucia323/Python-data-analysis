faccc505c802c616a15a6e7c60f3330ad7041c04
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-13 16:53:30 +0800

    Update 5.py

--- P109/5.py
+++ P109/5.py
@@ -1,5 +1,6 @@
 """ 找一段英文文本，统计该文本中单词的出现次数。比如“How are you. How are you.”的统计结果是{“how”:2，“are”:2，“you”:2} """
 str = "How are you. How are you."
+str = str.lower()
 l = []
 l = str.split(' ')
 result = {}
