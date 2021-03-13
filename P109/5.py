78c3644c67d14bad86f02643fcac2830937f3b45
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-13 14:48:31 +0800

    Create 5.py

---
+++ P109/5.py
@@ -0,0 +1,11 @@
+""" 找一段英文文本，统计该文本中单词的出现次数。比如“How are you. How are you.”的统计结果是{“how”:2，“are”:2，“you”:2} """
+str = "How are you. How are you."
+l = []
+l = str.split(' ')
+result = {}
+for word in l:
+    if word in result.keys():
+        result[word] += 1
+    else:
+        result[word] = 1
+print(result)
