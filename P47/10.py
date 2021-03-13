b30d5030c1f039c55267f0d11f2abf98f4c0c554
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-13 14:48:34 +0800

    Create 10.py

---
+++ P47/10.py
@@ -0,0 +1,4 @@
+""" 已知字符串：“Python is a widely used high-level, general-purpose, interpreted, dynamic programming language.“ 要求：将字符串中每个单词的第一个字母都变成大写字母，最终样式如下：
+'Python Is A Widely Used High-level, General-purpose, Interpreted, Dynamic Programming Language.' """
+str = "Python is a widely used high-lever, general-purpose, interpreted, dynamic programming language."
+print(str.title())
