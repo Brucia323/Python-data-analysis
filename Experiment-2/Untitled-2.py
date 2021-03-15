2009cde248073c617397dd3fcf48219be889c85f
周晨阳 <59002875+Aries0309@users.noreply.github.com>
2021-03-15 09:20:43 +0800

    Create Untitled-2.py

---
+++ Experiment-2/Untitled-2.py
@@ -0,0 +1,40 @@
+import random
+l = []
+
+
+def RandomList(n, min=1, max=100):
+    """生成随机数列表
+
+    Args:
+        n (int): 生成个数
+        min (int, optional): 最小值. Defaults to 1.
+        max (int, optional): 最大值. Defaults to 100.
+    """
+    for i in range(0, n):
+        l.append(random.randint(min, max))
+
+
+def PrimeNumber(x):
+    """判断是否素数
+
+    Args:
+        x (int): [description]
+
+    Returns:
+        bool: 素数返回True,合数返回False
+    """
+    for i in range(2, x):
+        if(x % i == 0):
+            return False
+    return True
+
+
+RandomList(100)
+print(l)
+sum = 0
+for i in range(0, 100):
+    result = PrimeNumber(l[i])
+    if(result == True):
+        print(l[i])
+        sum += l[i]
+print(sum)
