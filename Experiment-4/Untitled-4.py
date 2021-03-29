c1b63d337c2e2890d5be207426c6cdd82f58096b
周晨阳 <1448517833@qq.com>
2021-03-29 08:39:37 +0800

    Update Untitled-4.py

--- Experiment-4/Untitled-4.py
+++ Experiment-4/Untitled-4.py
@@ -1,11 +1,11 @@
 import numpy as np
 arr = np.arange(1000).reshape(100, 10)
 print(arr)
-arr1 = arr.T
+arr1 = arr.T  # 转置
 print(arr1)
-arr2 = np.dot(arr, arr1)
+arr2 = np.dot(arr, arr1)  # 内积
 print(arr2)
-arr3 = pow(arr, 2)
+arr3 = pow(arr, 2)  # 平方
 print(arr3)
-sum = arr3.sum()
+sum = arr3.sum()  # 平方和
 print(sum)
