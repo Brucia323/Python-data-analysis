ea6e7c6129558c7bf2bd6d4b6bc3e4eccd770c54
周晨阳 <1448517833@qq.com>
2021-04-19 16:57:37 +0800

    Update Untitled-1.py

--- Experiment-5/Untitled-1.py
+++ Experiment-5/Untitled-1.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.lib import math
-x = np.linspace(0, 2*math.pi)
+x = np.linspace(0, 2*math.pi)  # 在指定的间隔内返回均匀间隔的数字。
 plt.subplot(221)
-plt.plot(x, np.sin(x))
+plt.plot(x, np.sin(x))  # sin
 plt.subplot(222)
-plt.plot(x, np.cos(x))
+plt.plot(x, np.cos(x))  # cos
 plt.subplot(223)
-plt.plot(x, np.log(x))
+plt.plot(x, np.log(x))  # log
 plt.subplot(224)
-plt.plot(x, np.exp(x))
+plt.plot(x, np.exp(x))  # exp
 plt.show()
