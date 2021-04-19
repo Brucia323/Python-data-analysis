fd83802dc8f63be084076bcc831ac088f6613123
周晨阳 <1448517833@qq.com>
2021-04-19 16:57:43 +0800

    Update Untitled-4.py

--- Experiment-5/Untitled-4.py
+++ Experiment-5/Untitled-4.py
@@ -7,33 +7,35 @@ red1 = [0.91, 16.79, 66.14, 13.68, 2.12, 0.36]
 red2 = [10.32, 89.68]
 black1 = [1.76, 29.58, 60.92, 5.28, 1.41, 1.05]
 black2 = [24.42, 73.58]
-bar_width = 0.3
+bar_width = 0.3  # 宽度
 plt.subplot(221)
-index_red1 = arange(len(title1))
-index_black1 = index_red1+bar_width
-plt.bar(index_red1, height=red1, width=bar_width, color='red', label='红粉')
+index_red1 = arange(len(title1))  # 红粉年龄分布下标
+index_black1 = index_red1+bar_width  # 黑粉年龄分布下标
+plt.bar(index_red1, height=red1, width=bar_width,
+        color='red', label='红粉')  # 红粉年龄分布条形图
 plt.bar(index_black1, height=black1,
-        width=bar_width, color='black', label='黑粉')
-plt.legend()
-plt.xticks(index_red1+bar_width/2, title1)
+        width=bar_width, color='black', label='黑粉')  # 黑粉年龄分布条形图
+plt.legend()  # 图例
+plt.xticks(index_red1+bar_width/2, title1)  # x轴
 plt.subplot(222)
-index_red2 = arange(len(title2))
-index_black2 = index_red2+bar_width
-plt.bar(index_red2, height=red2, width=bar_width, color='red', label='红粉')
+index_red2 = arange(len(title2))  # 红粉性别分布下标
+index_black2 = index_red2+bar_width  # 黑粉性别分布下标
+plt.bar(index_red2, height=red2, width=bar_width,
+        color='red', label='红粉')  # 红粉性别分布条形图
 plt.bar(index_black2, height=black2,
-        width=bar_width, color='black', label='黑粉')
-plt.legend()
-plt.xticks(index_red2+bar_width/2, title2)
+        width=bar_width, color='black', label='黑粉')  # 黑粉性别分布条形图
+plt.legend()  # 图例
+plt.xticks(index_red2+bar_width/2, title2)  # x轴
 plt.subplot(245)
-plt.pie(red1, labels=title1)
+plt.pie(red1, labels=title1)  # 红粉年龄分布饼图
 plt.title('红粉')
 plt.subplot(246)
-plt.pie(black1, labels=title1)
+plt.pie(black1, labels=title1)  # 黑粉年龄分布饼图
 plt.title('黑粉')
 plt.subplot(247)
-plt.pie(red2, labels=title2)
+plt.pie(red2, labels=title2)  # 红粉性别分布饼图
 plt.title('红粉')
 plt.subplot(248)
-plt.pie(black2, labels=title2)
+plt.pie(black2, labels=title2)  # 黑粉性别分布饼图
 plt.title('黑粉')
 plt.show()
