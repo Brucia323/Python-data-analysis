f2182902678cd941b9f5f40fe69b0ce2818e9e84
周晨阳 <1448517833@qq.com>
2021-06-08 15:27:52 +0800

    Update Untitled-2.py

--- Experiment-9/Untitled-2.py
+++ Experiment-9/Untitled-2.py
@@ -1,15 +1,13 @@
 from pandas.io.parsers import read_csv
-from sklearn import naive_bayes, neighbors, svm
+from sklearn import datasets, model_selection, naive_bayes, neighbors, svm
 
-breast_cancer_wisconsin = read_csv("breast-cancer-wisconsin.data", names=[
-                                   '身份证号码', '半径', '纹理', '周长', '面积', '平滑度', '紧凑性', '凹度', '凹点', '对称性', '分形维数'])
-breast_cancer_wisconsin = breast_cancer_wisconsin.drop(['身份证号码'], axis=1)
-breast_cancer_wisconsin = breast_cancer_wisconsin.drop_duplicates()
-breast_cancer_wisconsin = breast_cancer_wisconsin.reset_index(drop=True)
+breast_cancer_wisconsin = datasets.load_breast_cancer()
+x_train, x_test, y_train, y_test = model_selection.train_test_split(
+    breast_cancer_wisconsin.data, breast_cancer_wisconsin.target, test_size=0.25)
 gaussian = naive_bayes.GaussianNB()
 bernoulli = naive_bayes.BernoulliNB()
 complement = naive_bayes.ComplementNB()
 categorical = naive_bayes.CategoricalNB()
 multinomial = naive_bayes.MultinomialNB()
-linearsvc=svm.LinearSVC()
-kneighborsClassifier=neighbors.KNeighborsClassifier()
+linearsvc = svm.LinearSVC()
+kneighborsClassifier = neighbors.KNeighborsClassifier()
